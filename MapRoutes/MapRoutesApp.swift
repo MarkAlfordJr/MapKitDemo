e752b86da78de8f044912c0b20180c6667dad190
Mark Alford Jr <markalfordjr@gmail.com>
2021-07-25 21:17:55 -0400

    Initial Commit

---
+++ MapRoutes/MapRoutesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapRoutesApp.swift
+//  MapRoutes
+//
+//  Created by Mark Alford on 7/25/21.
+//
+
+import SwiftUI
+
+@main
+struct MapRoutesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
